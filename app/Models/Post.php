59995cd1f247cd58c30109f6645f34a9eb0b8d17
noeldesalegn <desalen2121@gmail.com>
2024-11-04 07:39:04 -0800

    home controller

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 
 class Post extends Model
@@ -11,4 +12,8 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'body'];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\HomeController;
 Auth::routes();
 
 Route::get('/',[HomeController::class,'index'])->name('home.index');
+
 Route::get('/post',[HomeController::class,'post'])->name('home.post');
 Route::get('/about',[HomeController::class,'about'])->name('home.about');
 Route::get('/contact',[HomeController::class,'contact'])->name('home.contact');
